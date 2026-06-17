a233f5e355b04ef66bd34a231a5a31c08e39a970
SkinnyAstro <sartapeprasad@gmail.com>
2026-06-17 23:26:35 +0530

    Made some changes

--- src/test/Base/GetRequest.java
+++ src/test/Base/GetRequest.java
@@ -21,6 +21,7 @@ public class GetRequest {
     public void StatusCode(){
         given()
                 .header("Bearer","8dd257c7bd81cac870732d4ca9c75d90445cd0e3b380754aa0fdf7e38d0e912f")
+                .header("Accept","application/json")
                 .when()
                .get("/v2/users")
        .then()
