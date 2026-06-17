b059bd02a3eb497a909210ecc529c68db93d5b80
SkinnyAstro <sartapeprasad@gmail.com>
2026-06-17 23:22:46 +0530

    Added first code

---
+++ src/test/Base/GetRequest.java
@@ -0,0 +1,30 @@
+package Base;
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
+
+public class GetRequest {
+
+    @BeforeClass
+    public  static void Setup(){
+        RestAssured.baseURI = "https://gorest.co.in";
+        RestAssured.basePath = "/public";
+
+    }
+
+
+   @Test
+    public void StatusCode(){
+        given()
+                .header("Bearer","8dd257c7bd81cac870732d4ca9c75d90445cd0e3b380754aa0fdf7e38d0e912f")
+                .when()
+               .get("/v2/users")
+       .then()
+                .statusCode(200);
+
+    }
+}
