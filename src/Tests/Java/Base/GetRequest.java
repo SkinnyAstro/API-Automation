3c689bb5b5c2449c3b8b794ddd0719196d65f733
SkinnyAstro <sartapeprasad@gmail.com>
2026-06-19 14:25:37 +0530

    Created a seperate variable for token

--- src/Tests/Java/Base/GetRequest.java
+++ src/Tests/Java/Base/GetRequest.java
@@ -8,6 +8,8 @@ import static io.restassured.RestAssured.given;
 
 public class GetRequest {
 
+    String token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1NDExMyIsImVudGl0eVR5cGUiOiJJRCIsInJvbGVMaXN0IjpbIlJPTEVfQ1VTVE9NRVIiXSwiaXNzIjoiY29tLnRydWVtZWRzLmF1dGhfc2VydmljZSIsImV4cCI6MTc4MTkzMTcwNywiaWF0IjoxNzgxODQ1MzA3fQ.rqouqo0UI31EMSR8wogjftsDDJDHPbLT68GHmiEYIKEoES-E7rh9ERK5fJbcpNhIygUmz4K9qmS0VAodxwqH1L_vTXuRWLY5QSDEq_3QSYAKsNmZes5fPnUqvkeEqz8T7sqSPR9-LQciDooZStreOgNMp6AIatsddVufqaNx9e3cNQh-HVIbsQ_6vtjHT1m0yIHsLyO_WbUdOiUi0pQUgDvBwWFiPtli_2bSrw3Jg3YnHaOxI8BFZRQ8iosVJIoJCzD42oomvbLhr7mq6a15x8k2yBmiU5uSK2N6iAEVuaf5CdwOpMxry6r87-H0FyOYIej0CVky4zovH5ZOX-K3wg";
+
     @BeforeClass
     public  static void Setup(){
         RestAssured.baseURI = "https://gorest.co.in";
