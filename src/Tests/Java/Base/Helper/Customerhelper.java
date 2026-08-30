169a4afec83226d687fdbef65b37def4bc01fb67
SkinnyAstro <sartapeprasad@gmail.com>
2026-08-30 22:46:53 +0530

    Implemented the API client for the helper class

--- src/Tests/Java/Base/Helper/Customerhelper.java
+++ src/Tests/Java/Base/Helper/Customerhelper.java
@@ -1,24 +1,20 @@
 package Java.Base.Helper;
 
+import Java.Base.Base.ApiClient;
 import POJO.CustomerDetailsResponse;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.given;
 
-public class Customerhelper {
-
-   private String accesstoken;
+public class Customerhelper extends ApiClient {
 
     public Customerhelper(String accesstoken){
-       this.accesstoken = accesstoken;
+       super(accesstoken);
     }
 
     public CustomerDetailsResponse getCustomerDetails(){
-        return given()
-                .header("Authorization","Bearer " + accesstoken)
-                .contentType("application/json")
-                .log().ifValidationFails()
+        return getRequestSpec()
                 .when()
                 .post("/CustomerService/getCustomerDetails")
                 .then()
